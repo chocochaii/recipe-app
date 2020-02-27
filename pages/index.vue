eab69000a7ff0752f3b6a79079da964e28502ec7
chocochaii <thanat.apiwattanadej@gmail.com>
2020-02-27 18:14:41 +0700

    added app input and button components

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,56 @@
 <template>
-  <v-layout column justify-center align-center />
+  <v-layout column justify-center align-center>
+    <v-row>
+      <v-col>
+        <AppInput
+          v-model.trim="title"
+          placeholder="Recipe title"
+        />
+        <AppInput
+          ref="ingredientInput"
+          v-model.trim="ingredient"
+          placeholder="Add an ingredient"
+        />
+        <AppButton
+          :loading="isAdding"
+          :disabled="!title || !ingredients.length"
+          @click="onSubmit"
+        >
+          Add this recipe
+        </AppButton>
+      </v-col>
+    </v-row>
+  </v-layout>
 </template>
 
 <script>
-export default {}
+import AppInput from '~/components/UI/AppInput'
+import AppButton from '~/components/UI/AppButton'
+
+export default {
+  components: {
+    AppInput,
+    AppButton
+  },
+  data () {
+    return {
+      title: '',
+      ingredient: '',
+      ingredients: [],
+      isAdding: false
+    }
+  },
+  methods: {
+    onSubmit () {
+      this.isAdding = true
+      setTimeout(() => {
+        console.log('Added', {
+          title: this.title,
+          ingredients: this.ingredients
+        })
+        this.isAdding = false
+      }, 2000)
+    }
+  }
+}
 </script>
