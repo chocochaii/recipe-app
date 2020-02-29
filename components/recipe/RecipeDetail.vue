ef9922ac22cb78e0f03518e4557b4d5b489710b7
chocochaii <thanat.apiwattanadej@gmail.com>
2020-03-01 03:22:55 +0700

    added recipe detail page

---
+++ components/recipe/RecipeDetail.vue
@@ -0,0 +1,48 @@
+<template>
+  <article class="recipe-detail">
+    <v-card min-height="300px">
+      <v-card-title>{{ title }}</v-card-title>
+      <v-card-text>
+        <div class="d-flex flex-column">
+          <strong>Description:</strong>
+          <span>{{ description }}</span>
+          <br>
+          <strong>Ingredients:</strong>
+          <ul>
+            <li v-for="ingredient in ingredients" :key="ingredient">
+              {{ ingredient }}
+            </li>
+          </ul>
+        </div>
+      </v-card-text>
+    </v-card>
+  </article>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    ingredients: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.recipe-detail {
+  width: 100%;
+  max-width: 500px;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
---
+++ pages/recipes/_recipeId/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-layout justify-center align-center fill-height>
+    <RecipeDetail
+      :title="recipe.title"
+      :description="recipe.description"
+      :ingredients="recipe.ingredients"
+    />
+  </v-layout>
+</template>
+
+<script>
+import RecipeDetail from '~/components/recipe/RecipeDetail'
+
+export default {
+  components: {
+    RecipeDetail
+  },
+  data () {
+    return {
+      recipe: {
+        id: 'rcp1',
+        title: 'Cookie',
+        description: 'A delicious cookie',
+        ingredients: ['butter', 'milk', 'sugar']
+      }
+    }
+  }
+}
+</script>
