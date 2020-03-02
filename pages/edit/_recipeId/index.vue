afe9b4101c81a6ad70f8c286d0c910d40e0e5d25
chocochaii <thanat.apiwattanadej@gmail.com>
2020-03-02 08:17:43 +0700

    connect edit function to vuex

--- pages/edit/_recipeId/index.vue
+++ pages/edit/_recipeId/index.vue
@@ -6,24 +6,40 @@
 
 <script>
 import RecipeForm from '~/components/form/RecipeForm'
+import { fireDb } from '~/plugins/firebase'
 
 export default {
   components: {
     RecipeForm
   },
-  data () {
-    return {
-      recipe: {
-        id: 'rcp1',
-        title: 'Cookie',
-        description: 'A delicious cookie',
-        ingredients: ['butter', 'milk', 'sugar']
-      }
-    }
+  asyncData (context) {
+    return fireDb
+      .collection('recipes')
+      .doc(context.params.recipeId)
+      .get()
+      .then((ref) => {
+        if (ref.data() === undefined) {
+          context.error({ statusCode: 404, message: 'Recipe not found :(' })
+        }
+        return {
+          recipe: {
+            id: ref.id,
+            ...ref.data()
+          }
+        }
+      })
   },
   methods: {
     onSubmit (editedRecipe) {
-      console.log('on edit', editedRecipe)
+      this.$store.dispatch('editRecipe', editedRecipe)
+        .then(({ data, error }) => {
+          if (error) {
+            // eslint-disable-next-line
+            console.log(`Error: ${error}`)
+          } else {
+            this.$router.push(`/recipes/${data.id}`)
+          }
+        })
     }
   }
 }
